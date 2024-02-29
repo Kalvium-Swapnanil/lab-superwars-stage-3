8dbabe19b64389958d148c84fac2ecf99b1f41dc
Swapnanil Sarkar <you@example.com>
2024-02-29 12:09:38 +0530

    updated stage 3

--- src/app.js
+++ src/app.js
@@ -22,14 +22,26 @@ const PLAYERS = [
 ];
 
 // initialize players with image and strength
-const initPlayers = (players) => {
-    let detailedPlayers = '';
-
     // Instead of forloop use Map method
     // Code here
+    const initPlayers = (players) => {
+        let detailedPlayers = players.map(function (players, i) {
+          if (i % 2 === 0) {
+            var type = "hero";
+          } else {
+            var type = "villain";
+          }
+          const details = {
+            name: players,
+            strength: getRandomStrength(),
+            image: `images/super-${i + 1}.png`,
+            type: type,
+          };
+          return details;
+        });
 
     return detailedPlayers;
-}
+};
 
 // getting random strength
 const getRandomStrength = () => {
@@ -43,6 +55,41 @@ const buildPlayers = (players, type) => {
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
+    const hero = players.filter(function (obj) {
+        return obj.type === "hero";
+      });
+      const villain = players.filter(function (obj) {
+        return obj.type === "villain";
+      });
+      // filter players ends from here
+    
+      if (type === "hero") {
+        // creating a innerHTML array of each player
+        var x = hero.map(function (players) {
+          let frag = `<div class="player">
+          <img src="${players.image}" alt="">
+          <div class="name">${players.name}</div>
+          <div class="strength">${players.strength}</div>
+        </div>`;
+          // returning each innerHTML to frag
+          return frag;
+        });
+        // Joining all the elements in frag into string
+        fragment = x.join(" ");
+      } else {
+        // creating a innerHTML array of each player
+        var x = villain.map(function (players) {
+          let frag = `<div class="player">
+          <img src="${players.image}" alt="">
+          <div class="name">${players.name}</div>
+          <div class="strength">${players.strength}</div>
+        </div>`;
+          // returning each innerHTML to frag
+          return frag;
+        });
+        // Joining all the elements in frag into string
+        fragment = x.join(" ");
+      }
 
     return fragment;
 }
